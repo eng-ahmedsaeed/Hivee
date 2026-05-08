bf1c34d6bbfc4eff2b78352aa0ff31725f680b65
bonniesimmer <noura19918@gmail.com>
2026-05-08 18:57:57 +0300

    Made a main form with a navbar so we can

--- Hivee/Form1.cs
+++ Hivee/Form1.cs
@@ -2,42 +2,9 @@ namespace Hivee
 {
     public partial class Form1 : Form
     {
-        private int currentUserId;
-        private Form? activeForm = null;
-
-        public Form1(int loggedInUserId)
+        public Form1()
         {
             InitializeComponent();
-            currentUserId = loggedInUserId;
-        }
-
-        private void LoadFormIntoPanel(Form childForm)
-        {
-            if (activeForm != null)
-            {
-                activeForm.Close();
-            }
-
-            activeForm = childForm;
-
-            childForm.TopLevel = false;
-            childForm.FormBorderStyle = FormBorderStyle.None;
-            childForm.Dock = DockStyle.Fill;
-
-            pnlContent.Controls.Add(childForm);
-            pnlContent.Tag = childForm;
-            childForm.BringToFront();
-            childForm.Show();
-        }
-
-        private void btnInbox_Click(object sender, EventArgs e)
-        {
-            LoadFormIntoPanel(new Messages.InboxForm(currentUserId));
-        }
-
-        private void btnProfile_Click(object sender, EventArgs e)
-        {
-            
         }
     }
 }
