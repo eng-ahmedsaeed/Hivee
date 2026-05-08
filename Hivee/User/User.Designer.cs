075210a8ef3a37c6866e81fbbe533a51bbbee84b
do7amo7amed <dohahabeb15@gmail.com>
2026-05-08 17:30:05 +0300

    Add User Login and sign up Form

---
+++ Hivee/User/User.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace User
+{
+    partial class User
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            panel2 = new Panel();
+            label11 = new Label();
+            textBox8 = new TextBox();
+            button3 = new Button();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            button4 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(53, 13);
+            panel1.Margin = new Padding(4);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(822, 595);
+            panel1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ButtonFace;
+            button2.Font = new Font("Lucida Calligraphy", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.Highlight;
+            button2.Location = new Point(333, 472);
+            button2.Name = "button2";
+            button2.Size = new Size(119, 37);
+            button2.TabIndex = 6;
+            button2.Text = "Sign Up";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Location = new Point(292, 390);
+            button1.Name = "button1";
+            button1.Size = new Size(196, 51);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(211, 292);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(345, 34);
+            textBox2.TabIndex = 4;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(211, 166);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(345, 34);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(177, 239);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 28);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(171, 135);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 28);
+            label2.TabIndex = 1;
+            label2.Text = "Email";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial Rounded MT Bold", 25.8000011F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.Location = new Point(309, 29);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(143, 51);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            label1.Click += label1_Click;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(label11);
+            panel2.Controls.Add(textBox8);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(textBox7);
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(textBox5);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Location = new Point(53, 13);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(822, 595);
+            panel2.TabIndex = 7;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(171, 220);
+            label11.Name = "label11";
+            label11.Size = new Size(129, 28);
+            label11.TabIndex = 15;
+            label11.Text = "Date OF Birth";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(214, 174);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(458, 34);
+            textBox8.TabIndex = 14;
+            textBox8.TextChanged += textBox8_TextChanged;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ActiveCaption;
+            button3.Location = new Point(556, 526);
+            button3.Name = "button3";
+            button3.Size = new Size(154, 50);
+            button3.TabIndex = 13;
+            button3.Text = "Submit";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(211, 461);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(458, 34);
+            textBox7.TabIndex = 11;
+            textBox7.TextChanged += textBox7_TextChanged;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(211, 393);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(458, 34);
+            textBox6.TabIndex = 10;
+            textBox6.TextChanged += textBox6_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(211, 319);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(458, 34);
+            textBox5.TabIndex = 9;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(211, 251);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(458, 34);
+            textBox4.TabIndex = 8;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(211, 106);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(458, 34);
+            textBox3.TabIndex = 7;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(171, 416);
+            label10.Name = "label10";
+            label10.Size = new Size(0, 28);
+            label10.TabIndex = 6;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(174, 430);
+            label9.Name = "label9";
+            label9.Size = new Size(168, 28);
+            label9.TabIndex = 5;
+            label9.Text = "Confirm Password";
+            label9.Click += label9_Click;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(171, 356);
+            label8.Name = "label8";
+            label8.Size = new Size(93, 28);
+            label8.TabIndex = 4;
+            label8.Text = "Password";
+            label8.Click += label8_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(171, 288);
+            label7.Name = "label7";
+            label7.Size = new Size(59, 28);
+            label7.TabIndex = 3;
+            label7.Text = "Email";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(171, 143);
+            label6.Name = "label6";
+            label6.Size = new Size(103, 28);
+            label6.TabIndex = 2;
+            label6.Text = "Last Name";
+            label6.Click += label6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(171, 75);
+            label5.Name = "label5";
+            label5.Size = new Size(106, 28);
+            label5.TabIndex = 1;
+            label5.Text = "First Name";
+            label5.Click += label5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Rounded MT Bold", 25.8000011F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(297, 14);
+            label4.Name = "label4";
+            label4.Size = new Size(191, 51);
+            label4.TabIndex = 0;
+            label4.Text = "Sign UP";
+            label4.Click += label4_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.AliceBlue;
+            button4.Location = new Point(46, 526);
+            button4.Name = "button4";
+            button4.Size = new Size(254, 50);
+            button4.TabIndex = 16;
+            button4.Text = " I already have account ";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 28F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(938, 634);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label3;
+        private Label label2;
+        private Button button1;
+        private Button button2;
+        private Panel panel2;
+        private Label label4;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Button button3;
+        private TextBox textBox8;
+        private Label label11;
+        private Button button4;
+    }
+}
---
+++ Hivee/User/User.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace User
+{
+    public partial class User : Form
+    {
+        string connectionString = "Server=localhost;Database=SocialMedia;Integrated Security=True;TrustServerCertificate=True;";
+        public static int LoggedInUserId;
+        public User()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//login
+        {
+            SqlConnection con = new SqlConnection(connectionString);
+
+            con.Open();
+
+            string query = "LoginUser";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Email", textBox1.Text);
+            cmd.Parameters.AddWithValue("@Password", textBox2.Text);
+
+            object result = cmd.ExecuteScalar();
+
+            if (result != null)
+            {
+                LoggedInUserId = Convert.ToInt32(result);
+
+                MessageBox.Show("Login Successful!");
+            }
+            else
+            {
+                MessageBox.Show("Invalid Email or Password");
+            }
+
+            con.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)//sign up
+        {
+            panel1.Visible = false;
+            panel2.Visible = true;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e) //submit
+        {
+            if (textBox6.Text != textBox7.Text)
+            {
+                MessageBox.Show("Passwords do not match!");
+                return;
+            }
+
+            if (textBox6.Text.Length < 6)
+            {
+                MessageBox.Show("Password must be at least 6 characters/numbers.");
+                return;
+            }
+
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+
+                con.Open();
+
+                string query = "AddUser";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlCommand checkCmd = new SqlCommand("SELECT COUNT(*) FROM [User] WHERE Email=@Email", con);
+
+                checkCmd.Parameters.AddWithValue("@Email", textBox5.Text);
+
+                int exists = (int)checkCmd.ExecuteScalar();
+
+                if (exists > 0)
+                {
+                    MessageBox.Show("Email already exists!");
+                    return;
+                }
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@First_name", textBox3.Text);
+                cmd.Parameters.AddWithValue("@Last_name", textBox8.Text);
+                cmd.Parameters.AddWithValue("@Birth_date", DateTime.Parse(textBox4.Text));
+                cmd.Parameters.AddWithValue("@Email", textBox5.Text);
+                cmd.Parameters.AddWithValue("@Password", textBox6.Text);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Account Created Successfully!");
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            panel2.Visible = false;
+            panel1.Visible = true;
+
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+        }
+    }
+}
